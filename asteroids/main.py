83ff4da096c39845ad36921058f6684a32f02029
SirAjax <browski.rios@gmail.com>
2025-01-27 23:10:00 -0500

    added CircleShape file

--- asteroids/main.py
+++ asteroids/main.py
@@ -1,19 +1,25 @@
 from constants import *
 import pygame
 
+
 def main():
-    pygame.init()
     print("Starting asteroids!")
-    black = (0,0,0)
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    black = (0,0,0)
+    game_clock = pygame.time.Clock()
+    dt = 0
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(black, rect=None, special_flags=0) 
         pygame.display.flip()
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+
+        dt = game_clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
           main()
+
